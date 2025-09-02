b46008304a9ea4ddf7c179cf0a61b96a80bd23d2
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-09-03 00:16:50 +0600

    fix: Update Supabase credentials

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://wkgnowfheylpaoahsjap.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrZ25vd2ZoZXlscGFvYWhzamFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4MzYwMTUsImV4cCI6MjA3MjQxMjAxNX0.f4rFLwNWP9H_ZbPz7QQ-v0weE6aj5pc_jaoo5FkXtFE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
